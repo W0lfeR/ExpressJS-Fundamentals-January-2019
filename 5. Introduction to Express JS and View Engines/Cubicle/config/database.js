612c7e8d5e6c32ad5a6e5e71ad0d11b1457ee829
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-01-28 14:16:01 +0200

    Add console.log

--- 5. Introduction to Express JS and View Engines/Cubicle/config/database.js
+++ 5. Introduction to Express JS and View Engines/Cubicle/config/database.js
@@ -8,6 +8,7 @@ module.exports = config => {
     const db = mongoose.connection;
     db.once('open', err => {
         if (err) throw err;
+        console.log('Database ready');
     });
     db.on('error', reason => {
         console.log(reason);
