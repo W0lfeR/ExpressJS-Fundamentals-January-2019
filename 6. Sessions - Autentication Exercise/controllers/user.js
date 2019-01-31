df6095a4c3d976006a39da2c0d7a73c5e81835b7
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-01-31 15:32:57 +0200

    Update

--- 6. Sessions - Autentication Exercise/controllers/user.js
+++ 6. Sessions - Autentication Exercise/controllers/user.js
@@ -1,9 +1,26 @@
+const encryption = require('../util/encryption')
+
 module.exports = {
     registerGet: (req, res) => {
-        // TODO:
+       res.render('user/register')
     },
     registerPost: async (req, res) => {
-       // TODO:
+       const userBody = req.body
+       if(!userBody.username || !userBody.password || !userBody.repeatPassword){
+           userBody.error = 'Please fill all fields!'
+           res.render('user/register', userBody);
+           return; 
+       }
+       if(userBody.password !== userBody.repeatPassword){
+           userBody.error = 'Both passwords should match'
+           res.render('user/register', userBody);
+           return;
+       }
+const salt = encryption.generateSalt();
+const hashedPass 
+
+
+
     },
     logout: (req, res) => {
         // TODO:
