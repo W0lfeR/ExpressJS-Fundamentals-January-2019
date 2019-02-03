635fcbe75fc17796a2e00442b18b8542b8ded8f4
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-02-03 21:47:03 +0200

    Add files via upload

--- 6. Sessions - Autentication Exercise/models/User.js
+++ 6. Sessions - Autentication Exercise/models/User.js
@@ -2,12 +2,29 @@ const mongoose = require('mongoose');
 const encryption = require('../util/encryption');
 
 const userSchema = new mongoose.Schema({
-    username: { type: mongoose.Schema.Types.String, required: true, unique: true },
-    hashedPass: { type: mongoose.Schema.Types.String, required: true },
-    firstName: { type: mongoose.Schema.Types.String },
-    lastName: { type: mongoose.Schema.Types.String },
-    salt: { type: mongoose.Schema.Types.String, required: true },
-    roles: [{ type: mongoose.Schema.Types.String }]
+    username: {
+        type: mongoose.Schema.Types.String,
+        required: true,
+        unique: true
+    },
+    hashedPass: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    },
+    firstName: {
+        type: mongoose.Schema.Types.String
+    },
+    lastName: {
+        type: mongoose.Schema.Types.String
+    },
+    salt: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    },
+    roles: [{
+        type: mongoose.Schema.Types.String
+    }] //Admin, User, Student - As Array
+
 });
 
 userSchema.method({
@@ -17,5 +34,27 @@ userSchema.method({
 });
 
 const User = mongoose.model('User', userSchema);
-// TODO: Create an admin at initialization here
+User.seedAdmin = async () => {
+    try {
+        const users = await User.find();
+
+        if (users.length > 0) {
+            return;
+        }
+        const salt = encryption.generateSalt();
+        const hashedPass = encryption.generateHashedPassword(salt, 'Admin123');
+        return User.create({
+            username: 'Admin',
+            hashedPass: hashedPass,
+            firstName: 'Pesho',
+            lastName: 'G',
+            salt: salt,
+            roles: ['Admin']
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
 module.exports = User;
