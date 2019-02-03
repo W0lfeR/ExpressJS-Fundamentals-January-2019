821128d6ab914b7f517f0838a9968976311a8861
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-02-03 21:46:31 +0200

    Add files via upload

--- 6. Sessions - Autentication Exercise/controllers/user.js
+++ 6. Sessions - Autentication Exercise/controllers/user.js
@@ -2,22 +2,22 @@ const encryption = require('../util/encryption')
 
 module.exports = {
     registerGet: (req, res) => {
-       res.render('user/register')
+        res.render('user/register')
     },
     registerPost: async (req, res) => {
-       const userBody = req.body
-       if(!userBody.username || !userBody.password || !userBody.repeatPassword){
-           userBody.error = 'Please fill all fields!'
-           res.render('user/register', userBody);
-           return; 
-       }
-       if(userBody.password !== userBody.repeatPassword){
-           userBody.error = 'Both passwords should match'
-           res.render('user/register', userBody);
-           return;
-       }
-const salt = encryption.generateSalt();
-const hashedPass 
+        const userBody = req.body
+        if (!userBody.username || !userBody.password || !userBody.repeatPassword) {
+            userBody.error = 'Please fill all fields!'
+            res.render('user/register', userBody);
+            return;
+        }
+        if (userBody.password !== userBody.repeatPassword) {
+            userBody.error = 'Both passwords should match'
+            res.render('user/register', userBody);
+            return;
+        }
+        const salt = encryption.generateSalt();
+        const hashedPass
 
 
 
