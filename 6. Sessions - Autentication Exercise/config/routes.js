73937d323d48e7244be23e74108e518c432aec5e
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-02-04 22:21:20 +0200

    Add files via upload

--- 6. Sessions - Autentication Exercise/config/routes.js
+++ 6. Sessions - Autentication Exercise/config/routes.js
@@ -2,14 +2,25 @@ const restrictedPages = require('./auth');
 const router = require('express').Router();
 const homeController = require('../controllers/home');
 const userController = require('../controllers/user');
+const carController = require('../controllers/car');
+
 
 module.exports = app => {
     app.get('/', homeController.index);
     app.get('/user/register', restrictedPages.isAnonymous, userController.registerGet);
     app.post('/user/register', restrictedPages.isAnonymous, userController.registerPost);
-    app.get('/user/login', restrictedPages.isAnonymous, userController.loginGet);
-    app.post('/user/login', restrictedPages.isAnonymous, userController.loginGet);
-    app.post('/user/logout', restrictedPages.isAnonymous, userController.logout)
+    app.get('/user/login', userController.loginGet);
+    app.post('/user/login', userController.loginPost);
+    app.post('/user/logout', userController.logout);
+
+
+    app.get('/car/add', restrictedPages.hasRole('Admin'), carController.addGet);
+    app.post('/car/add', restrictedPages.hasRole('Admin'), carController.addPost);
+    app.get('/car/all', carController.allCars); // This route should be available for all. 
+    app.get('/car/rents', restrictedPages.isAuthed, carController.myRent);
+    app.get('/car/rent/:id', restrictedPages.isAuthed, carController.rent); // This route should be available for all registered users. Including admins.
+    app.get('/car/edit/:id', restrictedPages.isAuthed, carController.editGet);
+    app.post('/car/edit/:id', restrictedPages.isAuthed, carController.editPost);
 
     app.all('*', (req, res) => {
         res.status(404);
